47fb2de7317ce6a67b2c463ddd87b2e157e5b6f1
Elmabi <home@home.com>
2021-08-31 11:49:33 -0400

    Create AddAppointment Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { IoIosArchive } from "react-icons/io";
+import AddAppointment from "./components/AddAppointment";
 import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
+      <h1 className="text-5xl mb-3">
         <IoIosArchive className="inline-block text-red-400 align-top" /> Your
         Appointments
       </h1>
+      <AddAppointment />
       <Search />
     </div>
   );
