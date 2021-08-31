0204fe31fb1d2869ea71003e90d63e545cce4424
Elmabi <home@home.com>
2021-08-31 17:45:13 -0400

    Add scrolling to appointment list
    1 - Install tailwind scroll-bar hide
    2- Add scrolling design to appointment list
    3 - Hide scrollbar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import appointmentList from "./data.json";
 
 function App() {
   return (
-    <div className="App container mx-auto mt-3 font-thin bg-gray-50">
-      <h1 className="text-5xl mb-3">
-        <IoIosArchive className="inline-block text-red-400 align-top" /> Your
+    <div className="App container mx-auto my-20 font-thin bg-gray-50">
+      <h1 className="text-5xl mb-3 font-medium ">
+        <IoIosArchive className="inline-block text-red-400 align-top " /> Your
         Appointments
       </h1>
       <AddAppointment />
       <Search />
-      <ul className="divide-y divide-gray-200">
-        {appointmentList.map((appointment) => (
-          <AppointmentInfo appointment={appointment} key={appointment.id} />
-        ))}
-      </ul>
+      <div className="scrolling-auto rounded overflow-auto h-96 bg-gray-300 w-auto m-auto w-4 scrollbar-hide">
+        <ul className="divide-y divide-gray-200">
+          {appointmentList.map((appointment) => (
+            <AppointmentInfo appointment={appointment} key={appointment.id} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
